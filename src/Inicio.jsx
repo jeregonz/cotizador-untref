4a505917443e253908311e39c509cf0a34ce4826
Jeremias Gonzalez <jeregonza@hotmail.com.ar>
2023-11-03 16:20:01 -0300

    version beta

---
+++ src/Inicio.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import Formulario from "./componentes/Formulario";
+
+function Inicio () {
+    
+    return (
+        <>
+            <h1>Seguros del hogar</h1>
+            <Formulario/>
+            <Link to={"historial"}>
+                <button class="btn btn-primary">
+                    Ver el historial
+                </button>
+            </Link>
+        </>
+    )
+}
+
+export default Inicio
